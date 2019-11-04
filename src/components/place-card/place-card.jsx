17e80801a189cbc95181483b76b5c42e172839c3
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-11-03 20:41:30 -0500

    Adds offers data

--- src/components/place-card/place-card.jsx
+++ src/components/place-card/place-card.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const PlaceCard = (props) => {
-  const {placeName, onNameClick} = props;
+  const {place, onNameClick} = props;
+  const {name, type, price, priceText, picture} = place;
 
   return <article className="cities__place-card place-card">
     <div className="place-card__mark">
@@ -10,14 +11,14 @@ const PlaceCard = (props) => {
     </div>
     <div className="cities__image-wrapper place-card__image-wrapper">
       <a href="#">
-        <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image"/>
+        <img className="place-card__image" src={picture} width="260" height="200" alt="Place image"/>
       </a>
     </div>
     <div className="place-card__info">
       <div className="place-card__price-wrapper">
         <div className="place-card__price">
-          <b className="place-card__price-value">&euro;180</b>
-          <span className="place-card__price-text">&#47;&nbsp;night</span>
+          <b className="place-card__price-value">&euro;{price}</b>
+          <span className="place-card__price-text">&#47;&nbsp;{priceText}</span>
         </div>
         <button className="place-card__bookmark-button button" type="button">
           <svg className="place-card__bookmark-icon" width="18" height="19">
@@ -33,16 +34,16 @@ const PlaceCard = (props) => {
         </div>
       </div>
       <h2 className="place-card__name">
-        <a href="#" onClick={onNameClick}>{placeName}</a>
+        <a href="#" onClick={onNameClick}>{name}</a>
       </h2>
-      <p className="place-card__type">Apartment</p>
+      <p className="place-card__type">{type}</p>
     </div>
   </article>;
 };
 
 
 PlaceCard.propTypes = {
-  placeName: PropTypes.string.isRequired,
+  place: PropTypes.object.isRequired,
   onNameClick: PropTypes.func.isRequired,
 };
 
