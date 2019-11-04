c14dd3cd0a55282c323da42c749047cc1c67da3b
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-11-03 20:53:32 -0500

    Makes PlacesList a  PureComponent

--- src/components/places-list/places-list.jsx
+++ src/components/places-list/places-list.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import PlaceCard from "../place-card/place-card.jsx";
 
-const PlacesList = (props) => {
-  const places = props.places;
+export default class PlacesList extends PureComponent {
+  constructor() {
+    super();
 
-  return <div className="cities__places-list places__list tabs__content">
+    this.state = {
+      activePlace: -1
+    };
+  }
+
+  render() {
+    const places = this.props.places;
+
+    return <div className="cities__places-list places__list tabs__content">
                 ${places.map((place, index) => <PlaceCard placeName={place} onNameClick={onPlaceNameClick} key={index} />)}
-  </div>;
-};
+    </div>;
+  }
+}
 
 PlacesList.propTypes = {
   places: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-export default PlacesList;
