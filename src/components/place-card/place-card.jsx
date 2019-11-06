2701699303975998b83de31b80924c583de73908
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-11-05 22:31:28 -0500

    Fixes Premium badge and bookmarks

--- src/components/place-card/place-card.jsx
+++ src/components/place-card/place-card.jsx
@@ -7,7 +7,7 @@ const PlaceCard = (props) => {
   const {name, type, price, priceText, picture, isPremium, isBookmarked} = place;
 
   return <article className="cities__place-card place-card" onMouseOver={() => onHover(place)}>
-    <div className="place-card__mark" style={isPremium ? {display: `none`} : {}}>
+    <div className="place-card__mark" style={!isPremium ? {display: `none`} : {}}>
       <span>Premium</span>
     </div>
     <div className="cities__image-wrapper place-card__image-wrapper">
@@ -21,7 +21,7 @@ const PlaceCard = (props) => {
           <b className="place-card__price-value">&euro;{price}</b>
           <span className="place-card__price-text">&#47;&nbsp;{priceText}</span>
         </div>
-        <button className={`place-card__bookmark-button button` + isBookmarked ? `place-card__bookmark-button--active` : ``} type="button">
+        <button className={`place-card__bookmark-button button` + (isBookmarked ? ` place-card__bookmark-button--active` : ``)} type="button">
           <svg className="place-card__bookmark-icon" width="18" height="19">
             <use xlinkHref="#icon-bookmark"></use>
           </svg>
