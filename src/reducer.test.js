cad09101fe151d11eae23ebcd3fa681089521bb5
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-11-30 21:57:11 -0500

    Adds test for loadOffers

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,4 +1,6 @@
-import {ActionCreator, reducer} from "./reducer";
+import {ActionCreator, reducer, Operation} from "./reducer";
+import MockAdapter from "axios-mock-adapter";
+import {createAPI} from "./api";
 
 describe(`Action creators work correctly`, () => {
   it(`CHANGE_CITY changes city correctly`, () => {
@@ -66,5 +68,27 @@ describe(`Reducer works correctly`, () => {
       offersForCity: [{name: `1`, city: {name: `Moscow`}},
         {name: `2`, city: {name: `Moscow`}}]
     });
+  });
+});
+
+describe(`Data reducer works correctly`, () => {
+  it(`Should make a correct API call to /offers`, function () {
+    const dispatch = jest.fn();
+    const api = createAPI(dispatch);
+    const apiMock = new MockAdapter(api);
+    const offerLoader = Operation.loadOffers();
+
+    apiMock
+      .onGet(`/hotels`)
+      .reply(200, [{fake: true}]);
+
+    return offerLoader(dispatch, jest.fn(), api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: `LOAD_OFFERS`,
+          payload: [{fake: true}],
+        });
+      });
   });
 });
