926ca7e273e39e5b417e898a1c8353e797172034
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-12-05 13:31:54 -0500

    SignInForm: makes password required

--- src/components/sign-in-form/sign-in-form.jsx
+++ src/components/sign-in-form/sign-in-form.jsx
@@ -39,7 +39,7 @@ class SignInForm extends PureComponent {
                 </div>
                 <div className="login__input-wrapper form__input-wrapper">
                   <label className="visually-hidden">Password</label>
-                  <input className="login__input form__input" type="password" name="password" placeholder="Password" required=""/>
+                  <input className="login__input form__input" type="password" name="password" placeholder="Password" required/>
                 </div>
                 <button className="login__submit form__submit button" type="submit">Sign in</button>
               </form>
