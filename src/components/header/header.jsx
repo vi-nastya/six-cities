13e880f8541bccda9fce6730714d5ae0e30e8b20
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-12-10 09:46:49 -0500

    Fixes header logo link

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,9 +9,9 @@ const Header = (props) => {
     <div className="container">
       <div className="header__wrapper">
         <div className="header__left">
-          <a className="header__logo-link" href="/">
+          <Link className="header__logo-link" to="/">
             <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-          </a>
+          </Link>
         </div>
         <nav className="header__nav">
           <ul className="header__nav-list">
