0cd9bde220296715630711d4d7f56f169fe4e061
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-12-10 23:45:38 -0500

    Adds sorting component

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -4,6 +4,7 @@ import {ActionCreator} from "../../reducer";
 import PlacesList from "../places-list/places-list.jsx";
 import Map from "../map/map.jsx";
 import CitiesList from "../cities-list/cities-list.jsx";
+import Sorting from "../sorting/sorting.jsx";
 import {connect} from "react-redux";
 import {compose} from "recompose";
 import withActiveItem from "../../hocs/with-active-item/with-active-item.jsx";
@@ -44,21 +45,7 @@ const MainPage = (props) => {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offersForCity.length} places to stay in {city.name}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                  <li className="places__option" tabIndex="0">Price: low to high</li>
-                  <li className="places__option" tabIndex="0">Price: high to low</li>
-                  <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul>
-              </form>
+              <Sorting/>
               <PlacesList places={offersForCity} setActiveItem={setActiveItem}/>
             </section>
             <div className="cities__right-section">
