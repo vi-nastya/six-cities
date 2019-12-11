3df6df30b5a81ec95e954e4b9e30ab7daa094970
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-12-11 01:19:03 -0500

    Adds offer sorting

--- src/selectors/selectors.js
+++ src/selectors/selectors.js
@@ -3,9 +3,25 @@ import _ from "lodash";
 
 const MAX_NEARBY_PLACES = 3;
 
+const sortOffers = (offers, sortType) => {
+  switch (sortType) {
+    case `DEFAULT`:
+      return offers;
+    case `PRICE_ASC`:
+      return offers.sort((offer1, offer2) => (offer1.price > offer2.price) ? 1 : -1);
+    case `PRICE_DESC`:
+      return offers.sort((offer1, offer2) => (offer1.price < offer2.price) ? 1 : -1);
+    case `RATING`:
+      return offers.sort((offer1, offer2) => (offer1.rating < offer2.rating) ? 1 : -1);
+  }
+  return offers;
+};
+
 const getCity = (state) => state.city;
 const getOffers = (state) => state.offers;
 const getFavoriteOffers = (state) => state.favoriteOffers;
+const getSortType = (state) => state.sortType;
+
 
 export const getCitiesList = createSelector(
     [getOffers],
@@ -33,9 +49,9 @@ export const getNearbyPlaces = (offerId) => {
 };
 
 export const getOffersForCity = createSelector(
-    [getOffers, getCity],
-    (offers, city) => {
-      return offers.filter((offer) => offer.city.name === city.name);
+    [getOffers, getCity, getSortType],
+    (offers, city, sortType) => {
+      return sortOffers(offers.filter((offer) => offer.city.name === city.name), sortType.name);
     }
 );
 
