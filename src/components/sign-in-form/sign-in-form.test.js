72c4e0193b41359074aa7184e3a3d1ceee8e23ad
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-12-12 00:43:40 -0500

    Updates snapshot test for SignInForm

--- src/components/sign-in-form/sign-in-form.test.js
+++ src/components/sign-in-form/sign-in-form.test.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import toJSON from "enzyme-to-json";
+import renderer from "react-test-renderer";
 import SignInForm from "./sign-in-form";
 
-Enzyme.configure({adapter: new Adapter()});
+it(`SignInForm component is rendered correctly after relaunch`, () => {
+  const tree = renderer.create(<SignInForm onFormSubmit={jest.fn()}/>).toJSON();
 
-it(`Signin form is rendered correctly after relaunch`, () => {
-  const tree = shallow(<SignInForm onFormSubmit={jest.fn()}/>);
-
-  expect(toJSON(tree)).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });
