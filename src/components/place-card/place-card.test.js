231ab47ba820c7ab1721a829b3a30672a78b50ef
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-12-12 00:36:38 -0500

    Updates snapshot test for PlaceCard

--- src/components/place-card/place-card.test.js
+++ src/components/place-card/place-card.test.js
@@ -4,7 +4,13 @@ import PlaceCard from "./place-card";
 import {offers} from "../../mocks/offers";
 
 it(`Place Card is rendered correctly after relaunch`, () => {
-  const tree = renderer.create(<PlaceCard place={offers[0]} onHoverOn={jest.fn()} onHoverOff={jest.fn()}/>).toJSON();
+  const tree = renderer.create(<PlaceCard
+    place={offers[0]}
+    onHoverOn={jest.fn()}
+    onHoverOff={jest.fn()}
+    cardClass={`cities__place-card`}
+    imageClass={`cities__image-wrapper`}
+  />).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
