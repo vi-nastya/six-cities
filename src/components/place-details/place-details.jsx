6412285f81f42b0d87c931571073f125e6554c66
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-12-12 00:09:32 -0500

    Fixes ApiToApp converter

--- src/components/place-details/place-details.jsx
+++ src/components/place-details/place-details.jsx
@@ -105,7 +105,7 @@ class PlaceDetails extends PureComponent {
                   <h2 className="property__host-title">Meet the host</h2>
                   <div className="property__host-user user">
                     <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                      <img className="property__avatar user__avatar" src={`../${placeData.host.avatarUrl}`} width="74" height="74" alt="Host avatar"/>
+                      <img className="property__avatar user__avatar" src={`/${placeData.host.avatarUrl}`} width="74" height="74" alt="Host avatar"/>
                     </div>
                     <span className="property__user-name">
                       {placeData.host.name}
