5d0af9c95eeb049324761f266d7f900a2acc9f54
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-12-11 23:14:18 -0500

    Updates rating inputs rendering

--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -4,45 +4,24 @@ import PropTypes from "prop-types";
 const MIN_COMMENT_LENGTH = 50;
 const MAX_COMMENT_LENGTH = 300;
 
+const RATING_LABELS = [`perfect`, `good`, `not bad`, `badly`, `terribly`];
+
 const ReviewForm = (props) => {
   const {isValid, onInputChange, onFormSubmit, formRef} = props;
   return <form className="reviews__form form" action="#" method="post" ref={formRef} onSubmit={onFormSubmit}>
     <label className="reviews__label form__label" htmlFor="review">Your review</label>
     <div className="reviews__rating-form form__rating">
-      <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={onInputChange}/>
-      <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-        <svg className="form__star-image" width="37" height="33">
-          <use xlinkHref="#icon-star"></use>
-        </svg>
-      </label>
-
-      <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"/>
-      <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-        <svg className="form__star-image" width="37" height="33">
-          <use xlinkHref="#icon-star"></use>
-        </svg>
-      </label>
-
-      <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"/>
-      <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-        <svg className="form__star-image" width="37" height="33">
-          <use xlinkHref="#icon-star"></use>
-        </svg>
-      </label>
-
-      <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
-      <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-        <svg className="form__star-image" width="37" height="33">
-          <use xlinkHref="#icon-star"></use>
-        </svg>
-      </label>
-
-      <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
-      <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-        <svg className="form__star-image" width="37" height="33">
-          <use xlinkHref="#icon-star"></use>
-        </svg>
-      </label>
+      {RATING_LABELS.map((title, index) =>
+        <React.Fragment key={`rating-radio-${index}`}>
+          <input className="form__rating-input visually-hidden"
+            name="rating" value={`${5 - index}`} id={`${5 - index}-stars`} type="radio" onChange={onInputChange}/>
+          <label htmlFor={`${5 - index}-stars`} className="reviews__rating-label form__rating-label" title={title}>
+            <svg className="form__star-image" width="37" height="33">
+              <use xlinkHref="#icon-star"></use>
+            </svg>
+          </label>
+        </React.Fragment>
+      )}
     </div>
     <textarea className="reviews__textarea form__textarea" id="review"
       minLength={MIN_COMMENT_LENGTH} maxLength={MAX_COMMENT_LENGTH}
