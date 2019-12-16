254543fb984e3389e11067c921cc1a8e82abf361
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-12-16 12:49:18 -0500

    Makes places list a functional component (D12)

--- src/components/places-list/places-list.jsx
+++ src/components/places-list/places-list.jsx
@@ -1,34 +1,28 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {offerPropTypes} from "../../props-types-validation";
 import PlaceCard from "../place-card/place-card.jsx";
 import {PlaceCardType} from "../../constants";
 
-class PlacesList extends PureComponent {
-  constructor() {
-    super();
-  }
+const PlacesList = (props) => {
+  const {
+    places,
+    setActiveItem
+  } = props;
 
-  render() {
-    const {
-      places,
-      setActiveItem
-    } = this.props;
-
-    return <div className="cities__places-list places__list tabs__content">
-      {places.map((place, index) => <PlaceCard
-        place={place}
-        cardType={PlaceCardType.MAIN_PAGE}
-        onHoverOn={() => {
-          setActiveItem(index);
-        }}
-        onHoverOff={() => {
-          setActiveItem(-1);
-        }}
-        key={index} />)}
-    </div>;
-  }
-}
+  return <div className="cities__places-list places__list tabs__content">
+    {places.map((place, index) => <PlaceCard
+      place={place}
+      cardType={PlaceCardType.MAIN_PAGE}
+      onHoverOn={() => {
+        setActiveItem(index);
+      }}
+      onHoverOff={() => {
+        setActiveItem(-1);
+      }}
+      key={index} />)}
+  </div>;
+};
 
 PlacesList.propTypes = {
   places: PropTypes.arrayOf(offerPropTypes).isRequired,
